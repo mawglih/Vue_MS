a690af12ec51d6428e29e0a88b6c3b5fb3151103
Oleg Markoff <mawglih@gmail.com>
2017-09-08 00:04:10 -0400

    project2

---
+++ project/Section Code (Start)/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'BMW', price: 100 },
+    {id: 2, name: 'Mercedes', price: 120 },
+    {id: 3, name: 'MSFT', price: 230 },
+    {id: 4, name: 'Google', price: 230 }
+]
