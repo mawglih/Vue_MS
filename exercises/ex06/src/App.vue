5c56f4cea6e22bcd8d0d5843620c3d6f3ca92d8e
mawglih <mawglih@gmail.com>
2017-09-27 19:57:29 -0400

    sept27

---
+++ exercises/ex06/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+          <app-server-list></app-server-list>
+          <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import ServerList from '../components/ServerList.vue';
+import Footer from '../components/Footer.vue';
+import ServerDetails from '../components/ServerDetails.vue';
+export default {
+  components: {
+    appHeader: Header,
+    appServerList: ServerList,
+    appFooter: Footer,
+    appServerDetails: ServerDetails
+  }
+}
+</script>
+
+<style>
+
+</style>
